d617c9ed850eef9faf86648e8a9a5b22cf9aaaf5
Alvin Wan <hi@alvinwan.com>
2015-08-08 01:15:38 -0700

    fixed config

--- logic/config.py
+++ logic/config.py
@@ -10,17 +10,19 @@ Various setups for different app instances
 
 class Config:
 	"""Default config"""
-	
+
 	DEBUG = False
 	TESTING = False
-	PORT = 8000
-	HOST = '127.0.0.1'
-	SCHEME = 'http'
-	SECRET_KEY = 'flask+mongoengine=<3'
-	STATIC_PATH = 'static'
 	SESSION_STORE = 'session'
 	MONGODB_DB = 'default'
+	SECRET_KEY = 'flask+mongoengine=<3'
 	LIVE = ['v1']
+	STATIC_PATH = 'static'
+	
+	INIT = {
+		'port': 8001,
+		'host': '127.0.0.1',
+	}
 	
 	
 class ProductionConfig(Config):
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from logic import create_app
 app = create_app()
 
 if __name__ == "__main__":
-	app.run()
+	app.run(**app.config['INIT'])
