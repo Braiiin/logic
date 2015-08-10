8d4c1c74ed25efe258f35767074350e0437d7494
Alvin Wan <hi@alvinwan.com>
2015-08-09 17:48:17 -0700

    added test for core url_for

--- tests/v1/test_api.py
+++ tests/v1/test_api.py
@@ -1,3 +1,4 @@
+from flask import url_for
 from logic.v1.api import need
 from tests.v1.test_base import TestBase
 
@@ -10,3 +11,9 @@ class TestAPI(TestBase):
 		@need('put')
 		def endpoint():
 			pass
+		
+	def test_api_registered(self, app):
+		"""Tests that flask url_for works with api"""
+		with app.test_request_context('/'):
+			assert url_for('v1.user')
+			assert url_for('v1.user_path', path='user')
